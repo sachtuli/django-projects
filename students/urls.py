f9f659178b24343ed3c364f15858ec6dab7c45da
Sachin Tuli <sachintuli50@gmail.com>
2025-05-04 10:09:35 +0530

    student-project

---
+++ students/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for students project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from studentApp import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.StudentListView.as_view(), name="students"),
+    path("student/<int:pk>/", views.StudentDetailView.as_view(), name="details"),
+    path("student/create/", views.createStudentView.as_view()),
+    path("student/<int:pk>/update/", views.updateStudentView.as_view()),
+    path("student/<int:pk>/delete/", views.deleteStudentView.as_view()),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("logout/", views.logout_view),
+]
